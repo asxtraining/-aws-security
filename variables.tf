29725fd6b2404176a353ce83662fb9160bd58a7b
will-asxtraining <will@asxtraining.co.uk>
2021-04-17 18:44:41 +0100

    updated variables with details for asxtraining account

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_key_name" {
   description = "ssh key pair name"
-  default = "celidor-training"
+  default = "asx-training"
 }
 
 variable "aws_region" {
@@ -13,19 +13,19 @@ variable "environment" {}
 # Ubuntu Precise 12.04 LTS (x64)
 variable "aws_amis" {
   default = {
-    eu-west-1 = "ami-b1cf19c6"
-    us-east-1 = "ami-de7ab6b6"
-    us-west-1 = "ami-3f75767a"
-    us-west-2 = "ami-21f78e11"
+  eu-west-1 = "ami-ecbbb706"
+  us-east-1 = "ami-0565af6e282977273"
+  us-west-1 = "ami-0ff7f191316dba328"
+  us-west-2 = "ami-0ba5dfee72d5bb9a1"
   }
 }
 
 variable "zone_id" {
   description = "Route53 Zone ID"
-  default     = "Z1MX3O8HWEIIH4" # Zone ID for celidor.io
+  default     = "Z103269710HT8NFWHWHKA" # Zone ID for asxtraining.io
 }
 
 variable "domain" {
   description = "root domain"
-  default     = "celidor.io"
+  default     = "asxtraining.co.uk"
 }
