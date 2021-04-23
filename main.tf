eccf583e83173768f91828844c43eca32c772312
Dolsky, Frantisek <frantisek.dolsky@merck.com>
2021-04-23 14:37:20 +0200

    restricted SSH to single IP address

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_security_group" "aws-security" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["77.242.86.11/32"]
   }
 
   # HTTP access from the VPC
